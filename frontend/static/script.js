93db11bb51c830adff8dfd615d90b9808fd1242b
zxc93142819 <91686046+zxc93142819@users.noreply.github.com>
2024-07-13 22:24:02 +0800

    Update script.js

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,6 +1,6 @@
 // api_server = "https://youtdownload-899746c157cc.herokuapp.com:8000"
 // const port = process.env.PORT || 8000
-api_server = "http://0.0.0.0:8000"
+api_server = "https://yt-downloader-rkqk.onrender.com:8000"
 src_type = null; 
 video_id = null;
 
